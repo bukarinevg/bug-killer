f88d7e74d5a0af61d7f4f333c00c3de4747bd925
bukarinevgeni <bukarinevgeni@gmail.com>
2024-12-11 04:12:23 +0200

    Update output path in webpack config and add CI/CD workflow

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/main.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
     },
     module: {
         rules: [
