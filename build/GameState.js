079b95f2731f0707b10f854aa9041be4ff853c16
bukarinevg <bukarinevg@users.noreply.github.com>
2024-12-11 04:24:30 +0000

    deploy: 19704d9945b7a4044ad882f04df9fc9787ed0d1b

---
+++ build/GameState.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GameState = /** @class */ (function () {
+    function GameState(_a) {
+        var active = _a.active, scoreText = _a.scoreText, player = _a.player, cursors = _a.cursors, enemies = _a.enemies, pelletsLoop = _a.pelletsLoop, playerProjectile = _a.playerProjectile, enemyVelocity = _a.enemyVelocity, currentLevel = _a.currentLevel;
+        this.currentLevel = 1;
+        this.lostState = false;
+        console.log("active: ".concat(active));
+        this.active = active;
+        this.scoreText = scoreText;
+        this.player = player;
+        this.cursors = cursors;
+        this.enemies = enemies;
+        this.pelletsLoop = pelletsLoop;
+        this.playerProjectile = playerProjectile;
+        this.enemyVelocity = enemyVelocity;
+        this.currentLevel = currentLevel ? currentLevel : 1;
+    }
+    return GameState;
+}());
+exports.default = GameState;
